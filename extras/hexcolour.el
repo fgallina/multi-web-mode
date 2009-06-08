164ed3210f16bf20d659529ab6c8bdc4e18a6f35
Fabián Ezequiel Gallina <fgallina@caffeinegroup.com.ar>
2009-06-07 21:48:08 -0300

    added availability to increase or decrease values of hexadecimal numbers with M-up and M-down respectively

--- extras/hexcolour.el
+++ extras/hexcolour.el
@@ -2,6 +2,15 @@
 ;; http://www.emacswiki.org/emacs/HexColour
 
 (require 'cl)
+(require 'thingatpt)
+
+
+(defvar hexcolour-mode-map
+  (let ((hexcolour-mode-map (make-sparse-keymap)))
+    (define-key hexcolour-mode-map [(meta up)] 'inc-number-at-point)
+    (define-key hexcolour-mode-map [(meta down)] 'dec-number-at-point)
+    hexcolour-mode-map)
+  "Keymaps for hexcolour-mode")
 
 
 (defun hexcolour-luminance (color)
@@ -27,9 +36,58 @@
                         (:background ,colour)))))))))
 
 
+(put 'hexadecimal 'end-op
+     (lambda ()
+       (re-search-forward "[0-9a-fA-F]\\{3\\}[0-9a-fA-F]\\{3\\}?" nil t)))
+
+
+(put 'hexadecimal 'beginning-op
+     (lambda ()
+       (if (re-search-backward "#" nil t)
+           (forward-char))))
+
+
+(defun change-number-at-point (func)
+  (let* ((bounds (bounds-of-thing-at-point 'hexadecimal))
+         (number (buffer-substring
+                  (point-marker)
+                  (save-excursion
+                    (forward-char 1)
+                    (point-marker))))
+         (point (point)))
+    (when (and (>= point (car bounds))
+               (< point (cdr bounds)))
+      (goto-char (point))
+      (delete-char 1)
+      (insert (funcall func (string-to-number number 16)))
+      (goto-char point))))
+
+
+(defun inc-number-at-point ()
+  (interactive)
+  (change-number-at-point
+   (lambda (number)
+     (let ((number (+ number 1)))
+       (when (> number 15)
+         (setq number 0))
+       (format "%x" number)))))
+
+
+(defun dec-number-at-point ()
+  (interactive)
+  (change-number-at-point
+   (lambda (number)
+     (let ((number (- number 1)))
+       (when (< number 0)
+         (setq number 15))
+       (format "%x" number)))))
+
+
 (define-minor-mode hexcolour-mode
   "show the color of hexadecimal values in the background of the word containing the color"
   nil " HexColour" nil
+  (assq-delete-all 'hexcolour-mode minor-mode-map-alist)
+  (push (cons 'hexcolour-mode hexcolour-mode-map) minor-mode-map-alist)
   (hexcolour-add-to-font-lock))
 
 
