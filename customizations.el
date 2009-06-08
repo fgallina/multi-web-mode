18d6d48359f2b913adb5345330764ec3dac619b8
Fabián Ezequiel Gallina <fgallina@caffeinegroup.com.ar>
2009-06-07 21:46:51 -0300

    added customizations file

---
+++ customizations.el
@@ -0,0 +1,24 @@
+;; Add hexcolour to css mode
+(require 'hexcolour)
+(add-hook 'css-mode-hook
+          (lambda ()
+            (hexcolour-mode)))
+
+;; fixme mode
+(require 'fixme-mode)
+(fixme-mode)
+
+;; amazing tabkey2-mode
+(require 'tabkey2)
+(tabkey2-mode)
+
+;; PHP Mode Customizations
+(add-hook
+ 'php-mode-hook
+ (lambda ()
+   ;; Enhanced Fill Paragraph on phpdoc comments
+   (setq paragraph-separate "^[ \t]*\\(\\(/[/\\*]+\\)\\|\\(\\*+/\\)\\|\\(\\*?\\)\\|\\(\\*?[ \t]*@[[:alpha:]]+\\([ \t]+.*\\)?\\)\\)[ \t]*$")
+   ;; Flymake for php
+   (flymake-mode 1)
+   ;; Open flymake error menu with F5
+   (define-key php-mode-map [f5] 'flymake-display-err-menu-for-current-line)))
