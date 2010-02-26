9f2693e913d1391a145461fc0403ce1f9b836210
Fabián Ezequiel Gallina <fgallina@cuca>
2010-02-26 00:55:51 -0300

    * Added example configuration file

---
+++ mweb-example-config.el
@@ -0,0 +1,7 @@
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (espresso-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
