0c0d59daa0f9185d9812fc0b571b90bde0559aa1
alberto-a <alberto.linked@gmail.com>
2012-12-01 07:28:17 -0600

    Update mweb-example-config.el
    <script> by itself is also javascript.

--- mweb-example-config.el
+++ mweb-example-config.el
@@ -2,6 +2,7 @@
 (setq mweb-default-major-mode 'html-mode)
 (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
                   (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (js-mode "<script>" "</script>")
                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
 (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
 (multi-web-global-mode 1)
